19d66ed50a1bff1e8b0267ea1b4347e6c227cbf1
Cedric Louyot <cedric.louyot@gmail.com>
2022-02-21 17:45:04 +0100

    Models ok with validations and associations - Issue to delete User or Pig having a Rental

--- app/models/pig.rb
+++ app/models/pig.rb
@@ -1,6 +1,7 @@
 class Pig < ApplicationRecord
-  has_many :rentals
+  has_many :rentals, dependent: :nullify
   has_many :users, through: :rentals
+  belongs_to :user
 
   validates :name, presence: true, uniqueness: true
   validates :daily_price, presence: true
