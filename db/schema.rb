a7adabe6ea942bb4b35c56635447df3bfce1d289
Cedric Louyot <cedric.louyot@gmail.com>
2022-02-21 16:18:05 +0100

    Pig and Rental models without validations and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_150302) do
+ActiveRecord::Schema.define(version: 2022_02_21_151702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pigs", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "required_space"
+    t.integer "eat_capacity"
+    t.bigint "user_id", null: false
+    t.integer "daily_price"
+    t.index ["user_id"], name: "index_pigs_on_user_id"
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "pig_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pig_id"], name: "index_rentals_on_pig_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +50,7 @@ ActiveRecord::Schema.define(version: 2022_02_21_150302) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pigs", "users"
+  add_foreign_key "rentals", "pigs"
+  add_foreign_key "rentals", "users"
 end
