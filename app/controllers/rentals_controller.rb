5bb2e876089a9241ef31536afd6c85e1de3028f2
Cedric Louyot <cedric.louyot@gmail.com>
2022-02-22 15:13:50 +0100

    rentals new and create

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,14 +3,32 @@ class RentalsController < ApplicationController
   end
 
   def new
+    @pig = Pig.find(params[:pig_id])
+    @rental = Rental.new
   end
 
   def create
+    @rental = Rental.new(rental_params)
+    @rental.user = current_user
+    @rental.pig = Pig.find(params[:pig_id])
+    @rental.status = 'pending'
+    if @rental.save
+      redirect_to rentals_path
+    else
+      render :new
+      raise
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:user, :pig, :start_date, :end_date, :status)
   end
 end
