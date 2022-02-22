22a4fa233d4b96822c5c7192ecb7b0611894f149
Gala Thevenet <galathevenet@hotmail.com>
2022-02-22 17:50:58 +0100

    added links for navigation, corrected bugs in views and controllers, corrected seeds

--- app/controllers/farmers/pigs_controller.rb
+++ app/controllers/farmers/pigs_controller.rb
@@ -1,6 +1,7 @@
 class Farmers::PigsController < ApplicationController
+  before_action :set_pig, only: %i[edit update destroy]
   def index
-    @pigs = Pig.all
+    @pigs = Pig.where(user: current_user)
   end
 
   def new
@@ -11,27 +12,24 @@ class Farmers::PigsController < ApplicationController
     @pig = Pig.new(pig_params)
     @pig.user_id = current_user.id
     if @pig.save
-      redirect_to(farmers_pig_path(@pig.id))
+      redirect_to(pig_path(@pig))
     else
       render :new
     end
   end
 
   def edit
-    @pig = Pig.find(params[:id])
   end
 
   def update
-    @pig = Pig.find(params[:id])
     if @pig.update(pig_params)
-      redirect_to(farmers_pig_path(@pig.id))
+      redirect_to(pig_path(@pig))
     else
       render :new
     end
   end
 
   def destroy
-    @pig = Pig.find(params[:id])
     @pig.destroy
     redirect_to farmers_pigs_path
   end
@@ -41,5 +39,12 @@ class Farmers::PigsController < ApplicationController
   def pig_params
     params.require(:pig).permit(:name, :required_space, :eat_capacity, :daily_price)
   end
+
+  def set_pig
+    @pig = Pig.find(params[:id])
+  end
+
+
+
 
 end
