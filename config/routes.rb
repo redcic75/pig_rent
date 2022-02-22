db8825ff9ad25e67baa7ab161f142f7f448f90e3
Cedric Louyot <cedric.louyot@gmail.com>
2022-02-22 12:00:18 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :farmer do
+    resources :pigs, except: [:show]
+    resources :rentals, only: [:index]
+  end
+
+  resources :pigs, only: [:index, :show] do
+    resources :rentals, only: [:new, :create]
+  end
+
+  resources :rentals, only: [:index, :edit, :update]
 end
