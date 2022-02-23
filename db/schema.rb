08f4fb69a06e2f984c83b5340d398d52ca39a688
Cedric Louyot <cedric.louyot@gmail.com>
2022-02-23 12:22:14 +0100

    Pig model updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_101523) do
+ActiveRecord::Schema.define(version: 2022_02_23_112132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_02_23_101523) do
     t.integer "eat_capacity"
     t.bigint "user_id", null: false
     t.integer "daily_price"
+    t.boolean "active"
     t.index ["user_id"], name: "index_pigs_on_user_id"
   end
 
