8f3f7ed3792c9d3780769da747f453306ed87e8e
Marion Santos <marionsantos@outlook.fr>
2022-02-24 12:01:09 +0100

    rating column added and seed completed with ratin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_112956) do
+ActiveRecord::Schema.define(version: 2022_02_24_105512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2022_02_23_112956) do
     t.bigint "user_id", null: false
     t.integer "daily_price"
     t.boolean "active"
+    t.integer "rating"
     t.index ["user_id"], name: "index_pigs_on_user_id"
   end
 
